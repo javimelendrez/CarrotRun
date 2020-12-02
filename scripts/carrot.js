eae627b63b10aaa980e0609413e65762266a2767
Javier Melendrez <44476129+javimelendrez@users.noreply.github.com>
2020-12-02 14:34:45 -0800

    the carrot now jumps

--- scripts/carrot.js
+++ scripts/carrot.js
@@ -1,10 +1,23 @@
 class Carrot{
         constructor(){
             //carrot class has an x and a y
-        this.x = 50;
-        this.y = height - 50;
+        this.r = 50;
+        this.x = this.r;
+        this.y = height - this.r;
+        //we need the carrot to jump so it needs an up velocity
+        this.vy = 0;
+        this.gravity = 2; // it is a counter attacking force adjusting speed of jump
+
+    }
+    jump(){
+        this.vy = -25; //moves the jump up
+    }
+    move(){
+        this.y += this.vy;
+        this.vy += this.gravity;
+        this.y = constrain(this.y, 0, height - this.r);
     }
     show(){
-        rect(this.x,this.y, 50,50);
+        rect(this.x, this.y, this.r, this.r);
     }
 }
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -7,4 +7,11 @@ function draw(){
     background(220);
     //call the function to show the carrot
     carrot.show();
+    carrot.move();
+}
+//if the user press the space key then the jump function will be called
+function keyPressed(){
+    if(key == ' '){
+        carrot.jump();
+    }
 }
