67f1af0d4173de94ab867f25636590a9ce63cf03
Javier Melendrez <44476129+javimelendrez@users.noreply.github.com>
2020-12-07 18:16:39 -0800

    the bunny objects are now coming towards the bunny at random

---
+++ scripts/bunny.js
@@ -0,0 +1,16 @@
+
+class Bunny{
+
+    constructor(){
+        this.r = 100;
+        this.x = width;
+        this.y = height - this.r;
+    }
+    move(){
+        this.x -= 16; //the whole screen is scrolling by 3 pixels
+
+    }
+    show(){
+        rect(this.x, this.y, this.r, this.r);
+    }
+}
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,6 +1,7 @@
 let carrot; //make a carrot  variable
+let bunnies = []; //an array of bunnies
 function setup(){
-    createCanvas(600,450);
+    createCanvas(800,450);
     carrot = new Carrot();
 }
 function draw(){
@@ -8,6 +9,15 @@ function draw(){
     //call the function to show the carrot
     carrot.show();
     carrot.move();
+    //pick a random number between 0 and 1
+    if(random(1) < 0.01) {
+        var taco = new Bunny();
+        bunnies.push(taco); //random chance of adding a bunny
+    }
+    for( let b of bunnies){
+        b.move();
+        b.show();
+    }
 }
 //if the user press the space key then the jump function will be called
 function keyPressed(){
