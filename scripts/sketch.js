ba65e53806aa6a091e6c95edf27025cada0f7be8
Javier Melendrez <44476129+javimelendrez@users.noreply.github.com>
2020-12-07 20:27:18 -0800

    implementing restart button
    known bug
    *** mousedPressed is being called right away without being clicked***

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,6 +1,7 @@
 let carrot; //make a carrot  variable
 let bunnies = []; //an array of bunnies
 let score; //this is the score that is going to print on the screen
+let restart_button; //this is a variable that we will use to create the restart button
 function setup(){
     createCanvas(800,450);
     carrot = new Carrot();
@@ -26,6 +27,23 @@ function draw(){
             //we should probably print on the screen game over
             textSize(30); //size of the font
             text("Game Over: You were killed by a bunny!", 200, 225);
+            //now draw a button for the user to restart the game
+            restart_button = createButton("Restart"); //restart the game
+            restart_button.position(400,250);
+            // if (restart_button.mouseClicked()){
+            //     restart();
+            // }
+            //restart_button.mousePressedOver(restart());
+            // going to try using a text box instead
+            // textSize(15);
+            // text("Restart", 300, 260);
+            // if(mousePressedOver(true)){
+
+            // }
+            restart_button.onClick = function rest(){
+                console.log('click');
+                restart();
+            };
             noLoop();
         }
     }
@@ -35,4 +53,9 @@ function keyPressed(){
     if(key == ' '){
         carrot.jump();
     }
+}
+//creating a restart function for when the user clicks the restart button
+function restart(){
+    console.log('hey look you clicked the restart button');
+    document.location.reload(true);
 }
